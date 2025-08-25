4b1b0d5b9ba5fffd20b63d488aff0bc253f24663
misaka10987 <136697326+misaka10987@users.noreply.github.com>
2025-08-25 23:32:13 +0800

    refactor: separate operations w/ vanilla mc

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ pub mod pack;
 pub mod prelude;
 pub mod storage;
 pub mod user;
+pub mod vanilla;
 
 use std::{
     collections::HashMap,
@@ -21,9 +22,7 @@ use std::{
 use anyhow::anyhow;
 use clap::Parser;
 use indicatif::{FormattedDuration, ProgressState};
-use mc_launchermeta::{
-    VERSION_MANIFEST_URL, version::Version as McVersion, version_manifest::Manifest,
-};
+use mc_launchermeta::{version::Version as McVersion, version_manifest::Manifest};
 use reqwest::{Client, IntoUrl, Response};
 
 use semver::Version;
@@ -32,15 +31,10 @@ pub use prelude::*;
 use tokio::{
     fs::{File, copy, create_dir_all, remove_file, rename},
     sync::RwLock,
-    task::JoinSet,
 };
-use tracing::{Instrument, info};
 use tracing_indicatif::style::ProgressStyle;
 
-use crate::{
-    mc::{check_class, check_os},
-    storage::Storage,
-};
+use crate::storage::Storage;
 
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 
@@ -100,105 +94,6 @@ impl Creeper {
             return Ok(inst);
         }
         self.load_inst().await
-    }
-
-    async fn fetch_manifest(&self) -> anyhow::Result<&Manifest> {
-        info!("synchronizing minecraft version manifest");
-        let manifest = self.http_get(VERSION_MANIFEST_URL).await?.json().await?;
-        Ok(self.manifest.get_or_init(|| manifest))
-    }
-
-    pub async fn manifest(&self) -> anyhow::Result<&Manifest> {
-        if let Some(manifest) = self.manifest.get() {
-            return Ok(manifest);
-        }
-        self.fetch_manifest().await
-    }
-
-    async fn fetch_mc_version(&self, version: Version) -> anyhow::Result<McVersion> {
-        info!("synchronizing minecraft {version} version metadata");
-        let manifest = self.manifest().await?;
-        let url = manifest
-            .get_version(&version.to_string())
-            .ok_or(anyhow!("minecraft version {version} not found in manifest"))?
-            .url
-            .to_owned();
-        let mc_version = self.http_get(url).await?.json::<McVersion>().await?;
-        self.mc_version
-            .write()
-            .await
-            .insert(version, mc_version.clone());
-        Ok(mc_version)
-    }
-
-    pub async fn mc_version(&self, version: Version) -> anyhow::Result<McVersion> {
-        if let Some(mc_version) = self.mc_version.read().await.get(&version) {
-            return Ok(mc_version.clone());
-        }
-        self.fetch_mc_version(version).await
-    }
-
-    pub async fn download_mc_lib(&self, version: Version) -> anyhow::Result<()> {
-        let mc_version = self.mc_version(version).await?;
-
-        let arts = mc_version
-            // libraries
-            .libraries
-            .into_iter()
-            // apply the rules
-            .filter(|x| {
-                x.rules.as_ref().is_none_or(|rules| {
-                    rules.iter().all(|rule| {
-                        if !rule.features.is_empty() {
-                            todo!("does not support rules with features")
-                        }
-                        let os = rule.os.as_ref().is_none_or(check_os);
-                        match rule.action {
-                            mc_launchermeta::version::rule::RuleAction::Allow => os,
-                            mc_launchermeta::version::rule::RuleAction::Disallow => !os,
-                        }
-                    })
-                })
-            })
-            // entries with artifacts to download
-            .filter_map(|x| x.downloads)
-            // flatten list of artifacts
-            .flat_map(|x| {
-                x.classifiers
-                    .unwrap_or_default()
-                    .into_iter()
-                    .filter_map(|(class, art)| if check_class(&class) { Some(art) } else { None })
-                    .chain(x.artifact.into_iter())
-            })
-            // remove duplication
-            .map(|x| (x.sha1.clone(), x))
-            .collect::<HashMap<_, _>>();
-
-        info!("downloading {} library artifacts", arts.len());
-
-        let mut set = JoinSet::new();
-
-        for art in arts.into_values() {
-            let creeper = self.clone();
-            let fut = async move {
-                creeper
-                    .storage
-                    .download(
-                        art.path,
-                        art.url,
-                        Some(art.size),
-                        Some(art.sha1).map(Checksum::sha1),
-                    )
-                    .await
-            };
-            set.spawn(fut.in_current_span());
-        }
-
-        while let Some(res) = set.join_next().await {
-            res??;
-        }
-
-        Ok(())
     }
 }
 

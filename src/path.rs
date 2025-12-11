5e8ad28e3e1ef3a97314f24000b7c89c090acd7e
misaka10987 <misaka10987@outlook.com>
2025-12-11 13:13:48 +0800

    add documentation

--- src/path.rs
+++ src/path.rs
@@ -2,7 +2,9 @@ use std::path::PathBuf;
 
 use anyhow::anyhow;
 use tokio::fs::create_dir_all;
+use tracing::debug;
 
+/// The local data storage directory for the app.
 pub fn creeper_data_dir() -> anyhow::Result<PathBuf> {
     let dir = dirs::data_local_dir()
         .ok_or(anyhow!("missing local data directory"))?
@@ -10,6 +12,7 @@ pub fn creeper_data_dir() -> anyhow::Result<PathBuf> {
     Ok(dir)
 }
 
+/// The cache directory for the app.
 pub fn creeper_cache_dir() -> anyhow::Result<PathBuf> {
     let dir = dirs::cache_dir()
         .ok_or(anyhow!("missing cache directory"))?
@@ -17,12 +20,15 @@ pub fn creeper_cache_dir() -> anyhow::Result<PathBuf> {
     Ok(dir)
 }
 
+/// Directory for Minecraft instances managed by Creeper.
 pub fn creeper_mc_dir() -> anyhow::Result<PathBuf> {
     let dir = creeper_data_dir()?.join("minecraft");
     Ok(dir)
 }
 
+/// Initialize all necessary directories, creating if missing.
 pub async fn init_creeper_dirs() -> anyhow::Result<()> {
+    debug!("creating creeper directories if missing");
     for dir in [creeper_data_dir()?, creeper_cache_dir()?, creeper_mc_dir()?] {
         create_dir_all(dir).await?;
     }

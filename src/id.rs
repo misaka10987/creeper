caa62e978c6b0985065211ad56f73e477facf499
misaka10987 <136697326+misaka10987@users.noreply.github.com>
2026-04-07 22:18:51 +0800

    fix infinite recursion on display for `Id`

--- src/id.rs
+++ src/id.rs
@@ -98,6 +98,6 @@ impl FromStr for Id {
 
 impl Display for Id {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{self}")
+        write!(f, "{}", self.0)
     }
 }
